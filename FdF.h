1665981f57a83fa196a7e3b5709d8a22358fb98e
IlyaIvanovv <48183003+IlyaIvanovv@users.noreply.github.com>
2019-04-05 17:49:34 +0300

    Add files via upload

--- FdF.h
+++ FdF.h
@@ -10,12 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+#include "/usr/local/include/mlx.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include "libft/includes/libft.h"
+#define SIZE_X 1920
+#define SIZE_Y 1080
 
 typedef struct s_pixel
 {
@@ -37,5 +39,8 @@ typedef struct s_window
 	void	*mlx_ptr;
 	void	*win_ptr;
 }				t_window;
-
-//int     map_draw(t_map *map);
+int     map_draw(t_map *map, t_window *window);
+void    init_map_content(t_map *map, t_window *window);
+int     first_read_map(char *line, int fd, t_map *map);
+void    draw_line(t_pixel dot1, t_pixel dot2, t_window *window);
+int     main(int argc, char **argv);
