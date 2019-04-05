d240d0ed7ced17855c101ea7c6682e457a0753f6
IlyaIvanovv <48183003+IlyaIvanovv@users.noreply.github.com>
2019-04-05 17:49:26 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -12,48 +12,40 @@
 
 #include "FdF.h"
 
-//void draw_line(t_pixel *dot1, t_pixel *dot2, t_window *window)
-//{
-//	const int deltaX = abs(dot2->x - dot1->x);
-//	const int deltaY = abs(dot2->y - dot1->y);
-//	const int signX = dot1->x < dot2->x ? 1 : -1;
-//	const int signY = dot1->y < dot2->y ? 1 : -1;
-//	//
-//    int error = deltaX - deltaY;
-//	//
-//	mlx_pixel_put(window->mlx_ptr, window->win_ptr, dot2->x, dot2->y, 0xFFFFFF);
-//	while (dot1->x != dot2->x || dot1->y != dot2->y)
-//	{
-//		mlx_pixel_put(window->mlx_ptr, window->win_ptr, dot1->x, dot1->y, 0xFFFFFF);
-//        int error2 = error * 2;
-//
-//		if (error2 > -deltaY)
-//		{
-//			error -= deltaY;
-//			dot1->x += signX;
-//		}
-//		if (error2 < deltaX)
-//		{
-//			error += deltaX;
-//			dot1->y += signY;
-//		}
-//	}
-//}
+void draw_line(t_pixel dot1, t_pixel dot2, t_window *window)
+{
+    //t_pixel *one;
+    //t_pixel *two;
+	const int deltaX = abs(dot2.x - dot1.x);
+	const int deltaY = abs(dot2.y - dot1.y);
+	const int signX = dot1.x < dot2.x ? 1 : -1;
+	const int signY = dot1.y < dot2.y ? 1 : -1;
+	//
+    int error = deltaX - deltaY;
+	//
+	mlx_pixel_put(window->mlx_ptr, window->win_ptr, dot2.x, dot2.y, 0xFF6600);
+	while (dot1.x != dot2.x || dot1.y != dot2.y)
+	{
+		mlx_pixel_put(window->mlx_ptr, window->win_ptr, dot1.x, dot1.y, 0xFF6600);
+        int error2 = error * 2;
+
+		if (error2 > -deltaY)
+		{
+			error -= deltaY;
+			dot1.x += signX;
+		}
+		if (error2 < deltaX)
+		{
+			error += deltaX;
+			dot1.y += signY;
+		}
+	}
+}
 
 /*void ft_putchar(char c)
 {
 	write(1, &c, 1);
 }*/
-
-//int deal_key(int key, void *param)
-//{
-//	t_pixel *dot1;
-
-	///pixel = (t_pixel *)param;
-//	if (key == 53)
-//		exit(0);
-//	return (0);
-//}
 
 int first_read_map(char *line, int fd, t_map *map)
 {
@@ -65,40 +57,53 @@ int first_read_map(char *line, int fd, t_map *map)
 	return 1;
 }
 
-void    init_map_content(t_map *map)
+void    init_map_content(t_map *map, t_window *window)
 {
     int i;
     i = 0;
     map->matrix = (t_pixel *)malloc(map->width * map->height * sizeof(t_pixel));// map->pixel[i] OR map->pixel[i*map->width + j] => map->pixel.x = j  map->pixel.y = i
     while (i < map->width * map->height)
     {
-        map->matrix[i].x = i % map->width;
-        map->matrix[i].y = i / map->width;
+        map->matrix[i].x = i % map->width * 50 + SIZE_X / 2 - map->width * 50 / 2;
+        map->matrix[i].y = i / map->width * 50 + SIZE_Y / 2 - map->height * 50 / 2;
         map->matrix[i].z = 0;
-        map->matrix[i].color = 0xFFFFFF;
+        map->matrix[i].color = 0xFF6600;
         i++;
     }
-  //  map_draw(map);
+    map_draw(map, window);
 }
 
-//int     map_draw(t_map *map)
-//{
-  //  int i;
-   // i = 0;
-   // while (i <= map->matrix->x)
-   // {
+int     map_draw(t_map *map, t_window *window)
+{
+    int x;
+    int y;
 
-   // }
-
-//}
+    y = 0;
+    while (y < map->height)
+    {
+        x = 0;
+        while (x < map->width)
+        {
+            if (x + 1 < map->width)
+                draw_line(map->matrix[y * map->width + x], map->matrix[y * map->width + x + 1], window);
+            if (y + 1 < map->height)
+                draw_line(map->matrix[y * map->width + x], map->matrix[(y + 1) * map->width + x], window);
+            x++;
+        }
+        y++;
+    }
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
     int fd;
     char *line;
     t_map map;
+    t_window window;
     line = ft_strnew(1);
-    if (argc != 2) {
+    if (argc != 2)
+    {
         perror("No walid file");
         exit(0);
     }
@@ -110,15 +115,10 @@ int main(int argc, char **argv)
 		ft_putstr("warning");
 		close(fd);
 	}
-    init_map_content(&map);
+    window.mlx_ptr = mlx_init();
+    window.win_ptr = mlx_new_window(window.mlx_ptr, SIZE_X, SIZE_Y, "fdf 21 school");
+    // mlx_key_hook(window->win_ptr, deal_key, (void *)window);
+    init_map_content(&map, &window);
+    mlx_loop(window.mlx_ptr);
     return (0);
 }
-              
-/*
-{
-	window->mlx_ptr = mlx_init();
-	window->win_ptr = mlx_new_window(window->mlx_ptr, 1000, 1000, "mlx 42");
-	mlx_key_hook(window->win_ptr, deal_key, (void *)window);
-	mlx_loop(window->mlx_ptr);
-}
-*/
